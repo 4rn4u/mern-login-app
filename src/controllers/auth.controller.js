5f1e54c486d4198b28733004750191b4d7ec5b8a
4rn4u <vilx00@protonmail.com>
2024-03-19 20:01:20 +0100

    changes

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -62,5 +62,18 @@ export const logout = (req, res) => {
 }
 
 export const profile = (req, res) => {
-    res.send("profile")
+    const userFound = User.findById(req.user.id)
+    
+    if (!userFound) return res.status(400).json({
+        message: "User not found"
+    });
+
+    return res.json({
+        id: userFound._id,
+        username: userFound.username,
+        email: userFound.email,
+        createdAt: userFound.createdAt,
+        updatedAt: userFound.updatedAt,
+    })
+    res.send('profile')
 }
