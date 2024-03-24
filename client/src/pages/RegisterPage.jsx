2def0ac990c19453fd5965474fcd7f7bfb899fd0
4rn4u <vilx00@protonmail.com>
2024-03-24 19:35:31 +0100

    password

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -9,7 +9,7 @@ function RegisterPage() {
           {...register('username', { required: true })}
           />
         <input type="email" {...register('email', { required: true })}/>
-        <input type="password" {...register('passw', { required: true })}/>
+        <input type="password" {...register('password', { required: true })}/>
           </form>
       
       </div>
