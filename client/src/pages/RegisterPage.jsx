e106ae1ff80de18fb936ba3df543eba4e802f219
4rn4u <vilx00@protonmail.com>
2024-03-24 19:33:56 +0100

    username

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,10 +1,13 @@
 import {useForm} from 'react-hook-form'
 
 function RegisterPage() {
+  const {register} = useForm()
     return(
       <div>
           <form action="">
-        <input type="text" name="username"/>
+        <input type="text"
+          {...register('username', { required: true })}
+          />
         <input type="email" name="email"/>
         <input type="password" name="password"/>
           </form>
